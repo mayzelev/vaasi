6f6b16b17e055e43d9fd3c689fa2008092816448
Prytula Olexandr <liga5872@gmail.com>
2024-09-19 12:14:53 +0300

    refactoring footer, added mock data

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,10 @@
 import style from './Footer.module.css';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/icons/logo.png';
-import insta from '../../assets/icons/instagram.png';
-import telegram from '../../assets/icons/telegram.png';
 import callImg from '../../assets/icons/call.png';
 import messageImg from '../../assets/icons/message.png';
 import VButton from '../VButton';
+import { footerLinkRulesData, footerLinkPersonsData, footerSocialData } from './mockData';
 
 export default function Footer() {
     return (
@@ -26,50 +25,32 @@ export default function Footer() {
                     </div>
                     <div className={style.center}>
                         <ul className={style.links}>
-                            <li>
-                                <Link to="/about-us" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                                    Про нас
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/rules-for-using-vaasi-code" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                                    Правила користування кодами VAASI
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/privacy-policy" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                                    Політика конфіденційності
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/rules-for-using-site" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                                    Правила користування сайтом
-                                </Link>
-                            </li>
+                            {footerLinkRulesData.map(({ to, text }) => (
+                                <li key={to}>
+                                    <Link to={to} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+                                        {text}
+                                    </Link>
+                                </li>
+                            ))}
                         </ul>
                         <ul className={`${style.links} ${style.linksInfo}`}>
-                            <li>
-                                <Link to="/natural-persons" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                                    Фізичні особи інформація
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/legal-entities" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                                    Юридичні особи інформація
-                                </Link>
-                            </li>
+                            {footerLinkPersonsData.map(({ to, text }) => (
+                                <li key={to}>
+                                    <Link to={to} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+                                        {text}
+                                    </Link>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                     <div className={style.right}>
                         <div className={style.social}>
                             <span>Соціальні мережі</span>
-
-                            <Link to="/instagram">
-                                <img className={style.icons} src={insta} alt="Instagram" />
-                            </Link>
-                            <Link to="/telegram">
-                                <img className={style.icons} src={telegram} alt="Telegram" />
-                            </Link>
+                            {footerSocialData.map(({ src, href, alt }) => (
+                                <a key={alt} href={href}>
+                                    <img className={style.icons} src={src} alt={alt} />
+                                </a>
+                            ))}
                         </div>
 
                         <div className={style.contact}>
---
+++ src/components/Footer/mockData.js
@@ -0,0 +1,19 @@
+import insta from '../../assets/icons/instagram.png';
+import telegram from '../../assets/icons/telegram.png';
+
+export const footerLinkRulesData = [
+    { to: '/about-us', text: 'Про нас' },
+    { to: '/rules-for-using-vaasi-code', text: 'Правила користування кодами VAASI' },
+    { to: '/privacy-policy', text: 'Політика конфіденційності' },
+    { to: '/rules-for-using-site', text: 'Правила користування сайтом' }
+];
+
+export const footerLinkPersonsData = [
+    { to: '/natural-persons', text: 'Фізичні особи інформація' },
+    { to: '/legal-entities', text: 'Юридичні особи інформація' }
+];
+
+export const footerSocialData = [
+    { src: insta, href: '/', alt: 'instagram' },
+    { src: telegram, href: '/', alt: 'telegram' }
+];
